8188d33198a3d0f054fd10b8217dc60e1397f383
FariaParvinMegha <sk15chapainawabganj@gmail.com>
2020-10-05 00:38:10 +0600

    sixth_commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,35 @@
 // preloader///////////////
-$(window).on('load', function(){
+$(window).on('load', function () {
     $("#status").fadeOut();
     $("#preloader").delay(350).fadeOut('slow');
 });
 
 
-$(function() {
+// team///////////
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false
+    });
+});
 
 
-
+// progress bars/////////
+$(function () {
+    $("#progress-elements").waypoint(function () {
+        $(".progress-bar").each(function () {
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") + "%"
+            }, 1000);
+        });
+        this.destroy();
+    }, {
+        offset: 'bottom-in-view'
+    });
 });
